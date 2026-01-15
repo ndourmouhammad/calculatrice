1b5b20dab5cec6912dccf1f0d6039060cc7c4f49
ndourmouhammad <ndourmouhammad15@gmail.com>
2026-01-15 21:45:31 +0000

    Add Sonar

--- src/test/java/com/example/calculatrice/CalculatriceTest.java
+++ src/test/java/com/example/calculatrice/CalculatriceTest.java
@@ -4,18 +4,18 @@ import com.example.Calculatrice;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculatriceTest {
+class CalculatriceTest {
 
     // Méthode de test pour l'addition
     @Test
-    public void testSum() {
+    void testSum() {
         Calculatrice calc = new Calculatrice();
         assertEquals(5, calc.sum(2, 3), "2 + 3 doit être égal à 5");
     }
 
     // Méthode de test pour la soustraction
     @Test
-    public void testMinus() {
+    void testMinus() {
         Calculatrice calc = new Calculatrice();
         assertEquals(1, calc.minus(3, 2), "3 - 2 doit être égal à 1");
     }
