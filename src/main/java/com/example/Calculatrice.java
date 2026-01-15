663c0ea95831302176e0a87a6d790a230b30a16a
ndourmouhammad <ndourmouhammad15@gmail.com>
2026-01-15 17:18:23 +0000

    Initial commit - Java calculatrice with unit tests

---
+++ src/main/java/com/example/Calculatrice.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Calculatrice {
+
+    // Methode addition
+    public int sum(int a,int b){
+        return a+b;
+    }
+
+    // Methode soustraction
+    public int minus(int a,int b){
+        return a-b;
+    }
+}
